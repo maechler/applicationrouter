bb27654fbd05cfc8853184fa3344a2b4014bbfeb
Florian <florian@nexpose.ch>
2015-01-07 14:51:44 +0100

    implemented tcp router

--- WebApplicationRouter/Routing/TcpRouter.cs
+++ WebApplicationRouter/Routing/TcpRouter.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Threading;
 using System.Net.Sockets;
-using System.Threading.Tasks;
 using System.Text;
-using System.IO;
 
 namespace Marx.Net
 {
 	public class TcpRouter
 	{
-		TcpRoutingServer routingServer;
 		TcpClient externalClient;
+
+		TcpRoutingServer routingServer;
+
+		delegate byte[] ManipulateBuffer (byte[] buffer);
 
 		public TcpRouter (TcpRoutingServer routingServer, TcpClient externalClient)
 		{
@@ -18,62 +18,59 @@ namespace Marx.Net
 			this.routingServer = routingServer;
 		}
 
-		public async void StartRouter ()
+		public void StartRouter ()
 		{
-			Byte[] externalBuffer = new Byte[externalClient.ReceiveBufferSize];
-			Byte[] externalBufferCopy = new Byte[externalClient.ReceiveBufferSize];
+			var internalClient = new TcpClient ();
 
-			externalClient.Client.Receive (externalBuffer);
+			internalClient.Connect ("localhost", 8000);
 
-			//copy buffer
-			for (int i = 0; i < externalBuffer.Length; i++) {
-				externalBufferCopy [i] = externalBuffer [i];
+			internalClient.ReceiveBufferSize = externalClient.SendBufferSize;
+			externalClient.ReceiveBufferSize = internalClient.SendBufferSize;
+
+			var externalStream = externalClient.GetStream ();
+			var internalStream = internalClient.GetStream ();
+
+			var inputBuffer = new byte[externalClient.SendBufferSize];
+			var outputBuffer = new byte[internalClient.SendBufferSize];
+
+			while (externalClient.Connected && internalClient.Connected) {
+				if (externalStream.DataAvailable) {
+					CopyStream (externalStream, internalStream, inputBuffer, SpoofHostAddress);
+				}
+
+				if (internalStream.DataAvailable) {
+					CopyStream (internalStream, externalStream, outputBuffer);
+				}
 			}
 
-			var externalMessage = System.Text.Encoding.UTF8.GetString (externalBuffer);
-			
-			//call internal tcp server
-			var internalResult = await Task.Factory.StartNew (() => CallInternalTcpServer ("localhost", 3000, externalBufferCopy));
-
-			externalClient.Client.Send (internalResult);
-
-			var internalMessage = System.Text.Encoding.UTF8.GetString (internalResult);
-
-
-
-			Console.WriteLine ("connection closed");
-
+			internalClient.Close ();
 			externalClient.Close ();
 		}
 
-		Byte[] CallInternalTcpServer (String address, int port, Byte[] data)
-		{ 
-			var internalClient = new TcpClient ();
-			internalClient.Connect ("localhost", 3000);
-
-			Byte[] internalBuffer = new Byte[internalClient.ReceiveBufferSize];
-
-			var datacopy = new Byte[332];
-
-			int i = 0;
-			while (data [i] != 0x00 && i < datacopy.Length) {
-				datacopy [i] = data [i];
-				i++;
-			}
-
-			var fakeData = Encoding.UTF8.GetBytes ("GET /sys/login HTTP/1.1\r\nHost: localhost:8080\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-us\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5\r\n\r\n");
-			//internalClient.Client.Send (System.Text.Encoding.UTF8.GetBytes ("GET / HTTP/1.1\r\nHost: localhost:8080\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-us\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5\r\n\r\n"));
-			internalClient.Client.Send (datacopy);
-			 
-			internalClient.Client.Receive (internalBuffer);
-			internalClient.Close ();
-
-			var internalMessage = System.Text.Encoding.UTF8.GetString (internalBuffer);
-
-			return internalBuffer;
-
+		void CopyStream (NetworkStream input, NetworkStream output, byte[] buffer)
+		{
+			CopyStream (input, output, buffer, null);
 		}
 
+		void CopyStream (NetworkStream input, NetworkStream output, byte[] buffer, ManipulateBuffer manipulationMethod)
+		{
+			input.Read (buffer, 0, buffer.Length);
+			input.Flush ();
+
+			if (manipulationMethod != null)
+				buffer = manipulationMethod (buffer);
+
+			output.Write (buffer, 0, buffer.Length);
+			output.Flush ();
+		}
+
+		byte[] SpoofHostAddress (byte[] buffer)
+		{
+			String content = Encoding.UTF8.GetString (buffer);
+			content = content.Replace ("8080", "8000");
+
+			return Encoding.UTF8.GetBytes (content);
+		}
 	}
 }
 
